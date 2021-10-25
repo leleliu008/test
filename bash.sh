c97d9a7a2241b18a2f792d9425d8f3ac236fcb30
leleliu008 <leleliu008@gmail.com>
2021-10-26 04:46:51 +0800

    optimized

---
+++ bash.sh
@@ -0,0 +1,25 @@
+package set summary "Bourne-Again SHell, a UNIX command interpreter"
+package set webpage "https://www.gnu.org/software/bash"
+package set src.git "https://git.savannah.gnu.org/git/bash.git"
+package set src.url "https://mirrors.tuna.tsinghua.edu.cn/gnu/bash/bash-5.1.tar.gz"
+package set src.sum "cc012bc860406dcf42f64431bcd3d2fa7560c02915a601aba9cd597a39329baa"
+package set version "5.1"
+package set license "GPL-3.0-or-later"
+package set bsystem "configure"
+package set dep.pkg "readline ncurses"
+package set ccflags "-pie -fPIE"
+package set ldflags "-pie -fPIE"
+
+prepare() {
+    sed_in_place 's|$(BUILTINS_LIB) $(LIBRARIES)|-Wl,--Bstatic $(BUILTINS_LIB) $(LIBRARIES) -Wl,--Bdynamic|' Makefile.in
+}
+
+build() {
+    configure \
+        --with-curses \
+        --without-bash-malloc \
+        --disable-profiling \
+        --enable-readline \
+        --enable-select \
+        --enable-history
+}
