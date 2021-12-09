b17e9a4b6a986b4967149235ea49f9ed8a9d724f
leleliu008 <leleliu008@gmail.com>
2021-12-09 22:21:59 +0800

    optimized

--- libdatrie.sh
+++ libdatrie.sh
@@ -5,7 +5,7 @@ package set src.url "https://github.com/tlwg/libdatrie/releases/download/v0.2.13
 package set src.sum "12231bb2be2581a7f0fb9904092d24b0ed2a271a16835071ed97bed65267f4be"
 package set dep.pkg "libiconv"
 package set ldflags "-lcharset"
-package set bsystem "autotools"
+package set bsystem "configure"
 
 build0() {
     sed_in_place '/$(LN_S) trietool/d' "$SOURCE_DIR/tools/Makefile.in" &&
