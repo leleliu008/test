07f246683880927658010f9b5bb4d5ceae992799
leleliu008 <leleliu008@gmail.com>
2021-12-18 16:48:09 +0800

    optimized

--- fortune.sh
+++ fortune.sh
@@ -3,7 +3,7 @@ package set src.git "https://github.com/shlomif/fortune-mod.git"
 package set src.url "https://github.com/shlomif/fortune-mod/releases/download/fortune-mod-3.10.0/fortune-mod-3.10.0.tar.xz"
 package set src.sum "0d1ef7257a4e92968eb3298ff67c409250b1768c409e32f519e345e83fed0d37"
 package set dep.cmd "pkg-config perl libxslt"
-package set dep.plm "Path::Tiny App::Docmake"
+package set dep.plm "Path::Tiny App::Docmake App::XML::DocBook::Builder"
 package set bsystem "cmake"
 
 build0() {
