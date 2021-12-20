abe1ccd21886a031eff589e7a4988da585973281
leleliu008 <leleliu008@gmail.com>
2021-12-21 00:47:02 +0800

    optimized

--- fortune.sh
+++ fortune.sh
@@ -3,26 +3,26 @@ package set src.git "https://github.com/shlomif/fortune-mod.git"
 package set src.url "https://github.com/shlomif/fortune-mod/releases/download/fortune-mod-3.10.0/fortune-mod-3.10.0.tar.xz"
 package set src.sum "0d1ef7257a4e92968eb3298ff67c409250b1768c409e32f519e345e83fed0d37"
 package set dep.cmd "pkg-config perl>=5.30 xsltproc"
-package set dep.plm "Path::Tiny App::Docmake"
+package set dep.plm "App::Cpan Path::Tiny App::Docmake"
 package set bsystem "cmake"
 
 build0() {
-    if [ "$GITHUB_ACTIONS" = true ] ; then
-        brew install perl || return 1
-        cpan -i App::Cpan || return 1
-        export PATH="$HOME/perl5/bin:$PATH"
-    fi
-
-    for pm in $(__pmw_get_available_pm_list)
-    do
-        __pmw_install_the_given_package "$pm" docbook-xsl && break
-    done
-
     if command -v brew > /dev/null ; then
+        [ -d "$(brew --prefix docbook-xsl)" ] || run brew install docbook-xsl || return 1
         # http://www.xmlsoft.org/xmlcatalog_man.html
         # https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/docbook-xsl.rb
         export XML_CATALOG_FILES="$(brew --prefix)/etc/xml/catalog"
+        echo "XML_CATALOG_FILES=$XML_CATALOG_FILES"
+    else
+        for pm in $(__pmw_get_available_pm_list)
+        do
+            __pmw_install_the_given_package "$pm" docbook-xsl && break
+        done
     fi
 
     cmakew
+}
+
+build() {
+    build0
 }
