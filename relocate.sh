8cccf03889d10f759dd9cc2e20b7df45c2c292a1
leleliu008 <leleliu008@gmail.com>
2024-12-05 07:08:52 +0800

    optimized

--- relocate.sh
+++ relocate.sh
@@ -2,43 +2,31 @@
 
 set -e
 
-D="$(dirname "$1")"
-
-cd "$D"
-
 SYSROOT="$PWD/glibc/lib"
 DYNAMIC_LOADER="$SYSROOT/ld-linux-x86-64.so.2"
 
-for d in binutils
+cd "$1"
+
+fs="$(find -type f)"
+
+for f in $fs
 do
-    fs="$(find "$d" -type f)"
+    FILE_MAGIC="$(./xxd -u -p -l 4 "$f")"
 
-    export IFS='
-'
+    # http://www.sco.com/developers/gabi/latest/ch4.eheader.html
+    if [ "$FILE_MAGIC" = 7F454C46 ] ; then
+        echo "ELF file: $f"
 
-    for f in $fs
-    do
-        [ -f "$f" ] || continue
+        PT_INTERP="$(./patchelf --print-interpreter "$f" 2>/dev/null || true)"
 
-        FILE_MAGIC="$(./xxd -u -p -l 4 "$f")"
-
-        # http://www.sco.com/developers/gabi/latest/ch4.eheader.html
-        if [ "$FILE_MAGIC" = 7F454C46 ] ; then
-            echo "ELF file: $f"
-
-            PT_INTERP="$(./patchelf --print-interpreter "$f" 2>/dev/null || true)"
-
-            if [ -n "$PT_INTERP" ] ; then
-                ./patchelf --set-interpreter "$DYNAMIC_LOADER" "$f"
-            fi
-
-            DT_NEEDED="$(./patchelf --print-needed "$f" || true)"
-
-            if [ -n "$DT_NEEDED" ] ; then
-                ./patchelf --set-rpath "\$ORIGIN/../lib:$SYSROOT" "$f"
-            fi
+        if [ -n "$PT_INTERP" ] ; then
+            ./patchelf --set-interpreter "$DYNAMIC_LOADER" "$f"
         fi
-    done
 
-    unset IFS
+        DT_NEEDED="$(./patchelf --print-needed "$f" || true)"
+
+        if [ -n "$DT_NEEDED" ] ; then
+            ./patchelf --set-rpath "\$ORIGIN/../lib:$SYSROOT" "$f"
+        fi
+    fi
 done
